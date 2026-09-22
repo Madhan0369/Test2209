9c8c5b4b6f19d01259be83de8178bb104d363ba7
Madhan0369 <madhan.mahalingam@impigertech.com>
2026-09-22 12:54:21 +0530

    dev update

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,4 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+  console.log("dev");
+  
 });
